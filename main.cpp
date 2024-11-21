4c8ba2740e48b7e5db03b8d4c95dd4c4b3267a54
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-11-20 22:30:37 -0800

    Updated design with bird sounds

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cstdlib>  // Include for using the system()
+
+class Timer
+{
+public:
+    int totalSeconds;
+    Timer(int minutes) : totalSeconds(minutes * 60) {}  // Interesting constructor syntax
+
+    void start()
+    {
+        for (int i = totalSeconds; i > 0; i--)
+        {
+            int secondsCounter = i;
+            int minuteCounter = secondsCounter / 60;
+            std::cout << "\rMinutes:  " << minuteCounter << " Seconds: " << secondsCounter % 60 << std::flush;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+
+        std::cout << "\nTime is up! Good job!\n" << std::endl;
+        
+        system("paplay ~/data/bird.mp3");  // Replace /path/to/your/alarm.wav
+    }
+};
+
+int main()
+{
+    std::cout << "Pomodoro Timer started " << std::endl;
+    Timer pomodoro(1);
+    pomodoro.start();
+
+    return 0;
+}
