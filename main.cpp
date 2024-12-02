7651037c3a313035c5b797c64ba88f09055b0514
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-12-01 19:25:31 -0800

    polished product

--- main.cpp
+++ main.cpp
@@ -6,30 +6,48 @@
 class Timer
 {
 public:
-    int totalSeconds;
-    Timer(int minutes) : totalSeconds(minutes * 60) {}  // Interesting constructor syntax
+    double totalSeconds;
+    Timer(double minutes) : totalSeconds(minutes * 60) {}  // Interesting constructor syntax
 
     void start()
     {
-        for (int i = totalSeconds; i > 0; i--)
+        for (double i = totalSeconds; i > 0; i--)
         {
             int secondsCounter = i;
             int minuteCounter = secondsCounter / 60;
             std::cout << "\rMinutes:  " << minuteCounter << " Seconds: " << secondsCounter % 60 << std::flush;
             std::this_thread::sleep_for(std::chrono::seconds(1));
         }
-
-        std::cout << "\nTime is up! Good job!\n" << std::endl;
-        
+        std::cout << "\nEnd of timer " << std::endl; 
         system("paplay ~/data/piano.mp3");  // Replace /path/to/your/Alarm.wav
+        }
+};
+
+class Prompt
+{
+public:
+    int question = 0;
+
+    void proceede()
+    {
+        
+        std::cout << "The Pomodoro Timer has started, good luck and have fun!" << std::endl;
+        Timer pomodoro(25);
+        Timer ender(5);
+        pomodoro.start();        
+        std::cout << "5 minute break? Enter 1/YES 0/NO " << std::endl;
+        std::cin >> question;
+            if(question == 1)
+            {
+               ender.start();
+            }
+        std::cout << "Have a good day! " << std::endl;
     }
 };
 
 int main()
 {
-    std::cout << "Pomodoro Timer started " << std::endl;
-    Timer pomodoro(25);
-    pomodoro.start();
-
+    Prompt reloj;
+    reloj.proceede();
     return 0;
 }
